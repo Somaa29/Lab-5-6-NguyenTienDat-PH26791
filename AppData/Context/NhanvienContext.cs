ebccd4300dd283e219e752dda6ac7afb05896eca
DESKTOP-PTN36F3\Admin <datntph26791@fpt.edu.vn>
2023-06-08 18:14:21 +0700

    no message

---
+++ AppData/Context/NhanvienContext.cs
@@ -0,0 +1,34 @@
+﻿using AppData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppData.Context
+{
+    public class NhanvienContext : DbContext
+    {
+
+        public NhanvienContext()
+        {
+            
+        }
+        public NhanvienContext(DbContextOptions options): base(options)
+        {
+        }
+        public DbSet<NhanVien> NhanViens { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-PTN36F3\\SQLEXPRESS;Initial Catalog=bttlab56;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
